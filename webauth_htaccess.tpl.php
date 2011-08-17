82a2140d9ff285a8a653e89cb6fd8509accef4a8
Steve Rude <srude@stanford.edu>
2011-08-16 17:04:06 -0700

    udpates for clean urls

--- webauth_htaccess.tpl.php
+++ webauth_htaccess.tpl.php
@@ -1,13 +1,19 @@
 # $Id$
 
 AuthType WebAuth
-require valid-user
-
 WebAuthLdapAttribute displayName
 WebAuthLdapAttribute suAffiliation
 
+RewriteEngine on
+<?php print $rewrite_url; ?>
+
+# Auto-generated below this line. Changes will be overwritten.
+<?php if ($require_valid_user): ?>
+  require valid-user
+<?php else: ?>
+  require user <?php print $users; ?> 
+<?php endif; ?>
+
 <?php foreach ($groups as $group): ?>
-
-WebAuthLdapPrivgroup <?php print $group; ?>
-
+  WebAuthLdapPrivgroup <?php print $group . "\n"; ?>
 <?php endforeach; ?>
