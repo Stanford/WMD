1fe90a91167dfbfe5854360eabc52a5c62fe64a3
Josh Koenig <josh@getpantheon.com>
2011-09-14 15:30:16 -0700

    adjustment to strip REMOTE_ prefixes closes #46

--- default/check.php
+++ default/check.php
@@ -6,14 +6,18 @@
  * Returns webauth information to the Drupal webauth module
  * to verify that a webauth_at cookie is valid.
  */
-	
-foreach ($_SERVER as $key => $value) {
+
+// Don't operate directly on Superglobal.
+$vars = $_SERVER;
+
+foreach ($vars as $key => $value) {
+  $key = preg_replace('^REMOTE_', '', $key); // strip REMOTE_ prefixes
 	if (strtoupper(substr($key, 0, 8)) === 'WEBAUTH_') {
 		$key2 = strtolower(substr($key, 8));
 		header('wa_' . $key2 . ': ' . $value);
 	}
 }
-header('wa_remote_user: ' . $_SERVER['REMOTE_USER']);
+header('wa_remote_user: ' . $vars['REMOTE_USER']);
 
 print '<html><head><title>wa_check</title></head>';
 print '<body>';
