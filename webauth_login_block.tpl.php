8b7ba60588ba7305107d06f8db8d40a0c56cba9a
Marco Wise <marco.wise@stanford.edu>
2012-03-01 17:50:07 -0800

    updated styles for user login form and webauth link

--- webauth_login_block.tpl.php
+++ webauth_login_block.tpl.php
@@ -1,3 +1,3 @@
 <div id="webauth_login_url">
-  <a href="<?php print $login_url ?>"><?php print $login_text ?></a>
+  <a class="button" href="<?php print $login_url ?>"><?php print $login_text ?></a>
 </div>
